2c731734ffffa2e53e6b48fa6bfe81185a962d91
Mamadou9094 <chekhdia@gmail.com>
2021-04-26 17:03:57 +0200

    create and config database :rocket:

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/',router)
+app.all('*', (req, res)=>{
+    res.status(404).send({message: 'endpoint not found'})
+})
 
 app.listen(8080, ()=>{
     console.log('bienvenue a mon server')
